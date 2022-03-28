2b486520dc4ef4c0e74b2cb4f82206bb2109ea11
Beni Morales <i.benimorales@gmail.com>
2022-03-28 18:44:44 +0200

    Implemento mejoras

--- js/main.js
+++ js/main.js
@@ -2,5 +2,9 @@
 
 const menuLink = document.querySelectorAll('.menu__link');
 const inputMenu = document.getElementById('burger-activador');
+const submitBtn = document.getElementsByClassName('form__btn');
+const form = document.getElementsByTagName('form');
+const inputForm = document.querySelectorAll('#input-form');
 
+//Plegar el menú móvil cuando clickamos en un link
 menuLink.forEach(item => item.addEventListener('click', () => inputMenu.checked = !inputMenu.checked));
